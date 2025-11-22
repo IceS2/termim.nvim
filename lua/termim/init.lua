6bdb42938773b5f886754c08b026906d716342f3
Pablo Takara <pjt1991@gmail.com>
2025-11-22 23:03:58 +0100

    Support Floating Terminal
    Added support for simple floating terminals, without exposing
    configurations. Breaking change on API just to rename Fterm and FTerm to
    Tterm and TTerm, which I believe it makes more sense. Debatable!

--- lua/termim/init.lua
+++ lua/termim/init.lua
@@ -21,6 +21,27 @@ termim.keep_open = function()
     vim.api.nvim_command('augroup END')
 end
 
+local open_float = function(command)
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+  local win_opts = {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "single"
+  }
+
+  vim.api.nvim_open_win(buf, true, win_opts)
+  vim.cmd("terminal " .. command)
+end
+
 termim.open = function(command, split_dir, keep_open)
     if command == '' or command == nil then
         command = vim.o.shell
@@ -36,7 +57,11 @@ termim.open = function(command, split_dir, keep_open)
         termim.auto_close()
     end
 
+  if split_dir == "float" then
+    open_float(command)
+  else
     vim.cmd(split_dir .. ' term://' .. command)
+  end
 end
 
 return termim
